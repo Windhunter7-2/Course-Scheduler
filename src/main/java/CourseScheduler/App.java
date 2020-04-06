dbb6be73d066849dbfd76f74692207b02c4a4066
ndawg <ndigdog@gmail.com>
2020-04-06 13:46:04 -0400

    Add JavaFX dependency, edit App.java

--- src/main/java/CourseScheduler/App.java
+++ src/main/java/CourseScheduler/App.java
@@ -3,12 +3,26 @@
  */
 package CourseScheduler;
 
-public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+public class App extends Application {
+    
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        launch();
     }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
 }
