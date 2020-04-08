8d17d5304c1ca9b1b10cda79cfec15a121d0ed82
princeolass <princeolass@yahoo.comm>
2020-04-08 16:59:58 -0400

    updated profile class

--- src/main/java/CourseScheduler/Profile.java
+++ src/main/java/CourseScheduler/Profile.java
@@ -4,29 +4,94 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Profile {
-
+    Database db;
+    String name;
+    int numcredits;
+    int numsemesters;
+    ArrayList<String> neededCourses;
+    ArrayList<String> doneCourses;
     public Profile(String Name){
-        String name = Name;
-
+         name = Name;
+         db= new Database(name);//users database
+         numcredits=18;
+         numsemesters=8;
     }
 
+    /**
+     * sets the user's profile name
+     * @param name user's name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public int setNumCredits(int credits){
-        int numcredits= credits;
+    ProfileDB userprofiledb = new ProfileDB(db);
+
+    /**
+     * sets user's choice of credits
+     * @param credits overall credits.
+     */
+    public void setNumCredits(int credits){
+        this.numcredits= credits;
+     }
+
+    /**
+     * returns numcredits
+     * @return numcredits
+     */
+    public int getNumcredits() {
         return numcredits;
     }
 
+    /**
+     * sets number of semesters
+     * @param semesters numsemesters
+     */
+    public void setNumSemesters(int semesters){
+        this.numsemesters= semesters;
 
-    public int setNumSemesters(int semesters){
-        int numSemesters= semesters;
-        return numSemesters;
-    }
-    public List<Course> neededCourses(){
-        return new ArrayList<Course>();
-    }
-
-    public List<Course> doneCourses(){
-        return new ArrayList<Course>();
     }
 
+    /**
+     * number of semesters.
+     * @return number of semesters.
+     */
+    public int getNumsemesters() {
+        return numsemesters;
+    }
+
+    /**
+     * adds new courses to needed list
+     * @param course course to add
+     * @return list of needed courses.
+     */
+    public ArrayList<String> addneededCourses(String course){
+        neededCourses.add(course);
+        return neededCourses;
+    }
+    /**
+     * adds done courses to done list
+     * @param course course to add
+     * @return list of done courses.
+     */
+    public ArrayList<String> adddoneCourses(String course){
+        doneCourses.add(course);
+        return doneCourses;
+    }
+
+    /**
+     * gets needed courses
+     * @return neededcourses
+     */
+    public ArrayList<String> getNeededCourses() {
+        return neededCourses;
+    }
+
+    /**
+     * gets done courses.
+     * @return donecourses
+     */
+    public ArrayList<String> getDoneCourses() {
+        return doneCourses;
+    }
 }
