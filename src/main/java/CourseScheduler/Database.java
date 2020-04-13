2ca3e90e22f716431926fe70a11a443e9c810690
ndawg <ndigdog@gmail.com>
2020-04-13 12:23:46 -0400

    Imports

--- src/main/java/CourseScheduler/Database.java
+++ src/main/java/CourseScheduler/Database.java
@@ -2,7 +2,6 @@ package CourseScheduler;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
