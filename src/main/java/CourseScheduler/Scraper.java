706bc5b63d172ea8852410a22e2f7fb91b0da98d
ndawg <ndigdog@gmail.com>
2020-04-14 17:27:58 -0400

    Remove unused import

--- src/main/java/CourseScheduler/Scraper.java
+++ src/main/java/CourseScheduler/Scraper.java
@@ -7,7 +7,6 @@ import org.jsoup.nodes.Element;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
