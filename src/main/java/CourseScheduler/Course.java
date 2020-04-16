73c32d8b6d75452bb3f907ad97885ec124655a3b
Evan Baker <chessmanevan@gmail.com>
2020-04-15 22:05:59 -0400

    Updated Course.java to have private fields

--- src/main/java/CourseScheduler/Course.java
+++ src/main/java/CourseScheduler/Course.java
@@ -1,50 +1,60 @@
 package CourseScheduler;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Course {
 	
 	/**
 	 * The full name of the course, eg "Software Engineering"
 	 */
-	public final String fullName;
+	private final String fullName;
+	
 	/**
 	 * The shortened name of the course, eg "CS 321"
 	 */
-	public final String name;
+	private final String name;
+	
 	/**
 	 * The type of the course, eg "CS" or "MATH" etc
 	 */
-	public final String type;
+	private final String type;
+	
 	/**
 	 * The number of credits that this course is worth
 	 */
-	public final int credits;
+	private final int credits;
+	
 	/**
 	 * The description of the course from the catalog
 	 */
-	public final String desc;
+	private final String desc;
+	
 	/**
-	 * The informal code for the course, which combines the type and number, eg "CS321"
+	 * The informal code for the course, which combines the type and number, eg "CS-321"
 	 */
-	public final String code;
+	private final String code;
+	
 	/**
 	 * A list of course codes that are immediate prerequisites for this course. This list might overlap
 	 * with some of the codes in coreqs.
 	 */
-	public final List<String> prerequisites;
+	private final List<String> prerequisites;
+	
 	/**
 	 * A list of course codes that are required, but can be taken alongside this course
 	 */
-	final List<String> coreqs;
+	private final List<String> coreqs;
+	
 	/**
 	 * An internal representation of flags used to process the course in the algorithm
 	 */
-	public int flag;
+	private int flag;
+	
 	/**
 	 * A string representation of the parents, eg "(CS110 & MATH125) | MATHXYZ"
 	 */
-	final String parents;
+	private final String parents;
 	
 	public Course(String fullName, String name, String type, int credits, String desc, String code, List<String> prereqs, List<String> coreqs, int flag, String parents) {
 		this.fullName = fullName;
@@ -118,5 +128,31 @@ public class Course {
 		sb.append(", flag=").append(flag);
 		sb.append('}');
 		return sb.toString();
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Course course = (Course) o;
+		return credits == course.credits &&
+				flag == course.flag &&
+				Objects.equals(fullName, course.fullName) &&
+				Objects.equals(name, course.name) &&
+				Objects.equals(type, course.type) &&
+				Objects.equals(desc, course.desc) &&
+				Objects.equals(code, course.code) &&
+				Objects.equals(prerequisites, course.prerequisites) &&
+				Objects.equals(coreqs, course.coreqs) &&
+				Objects.equals(parents, course.parents);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(fullName, name, type, credits, desc, code, prerequisites, coreqs, flag, parents);
 	}
 }
