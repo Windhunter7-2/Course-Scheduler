79ed11479339fa475eb234b78da594ff3cc33993
Evan Baker <chessmanevan@gmail.com>
2020-04-19 14:47:07 -0400

    Implemented the stuff for Semester.java

--- src/main/java/CourseScheduler/Semester.java
+++ src/main/java/CourseScheduler/Semester.java
@@ -1,5 +1,28 @@
-package CourseScheduler;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Semester {
+	
+	/**
+	 * Constructor
+	 */
+	public Semester()
+	{
+		semesterCourses = new ArrayList<Course>();
+	}
+	
+	/**
+	 * This is an array (*In order*) of the Courses for a particular semester chosen.
+	 */
+	private List<Course> semesterCourses;
+	
+	/**
+	 * Getter for Semester
+	 * @return semesterCourses
+	 */
+	public List<Course> getSemester()
+	{
+		return semesterCourses;
+	}
 
 }
