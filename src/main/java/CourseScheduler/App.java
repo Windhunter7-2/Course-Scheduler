6a41f405d69f6ca1c4f92d3c40772b78c814c6c9
ndawg <ndigdog@gmail.com>
2020-04-22 12:49:32 -0400

    Configure shadow jar

---
+++ src/main/java/CourseScheduler/App.java
@@ -0,0 +1,12 @@
+package CourseScheduler;
+
+public class App {
+	
+	/**
+	 * Main launching endpoint which prevents JavaFX runtime problems.
+	 */
+	public static void main(String[] args) throws Exception {
+		CourseScheduler.main(args);
+	}
+	
+}
