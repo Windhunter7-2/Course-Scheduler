05f86b9b1b8976529d3b8961e19e158ada5a38e7
jackmb <40642637+jackmb@users.noreply.github.com>
2020-04-22 01:04:19 -0400

    JackTests.testGetLastRun() works now

--- src/test/java/CourseScheduler/JackTests.java
+++ src/test/java/CourseScheduler/JackTests.java
@@ -2,34 +2,35 @@ package CourseScheduler;
 
 import org.junit.Test;
 
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.sql.SQLException;
+import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
 
 public class JackTests {
 
     @Test
     public void testGetLastRun() throws IOException {
         Scraper scr = new Scraper(new Catalog(new Database("testdb")));
-        LocalDateTime actual = LocalDateTime.now();
-        try {
-            actual = scr.getLastRun();
-        } catch(FileNotFoundException fnfe) {
-            fnfe.printStackTrace();
-        }
-        assertEquals(LocalDateTime.parse("11/22/1963, 12:30:00", DateTimeFormatter.ofPattern("MM/dd/yyyy, HH:mm:ss")), actual);
+        //Preserve the actual time the scraper was last run.
+        File dateFile = LocalStorage.get("dateLastRun.txt");
+        BufferedReader br = new BufferedReader(new FileReader(dateFile));
+        BufferedReader br2 = br;
+        String realLastRun = br.readLine();
+        //Update the stored time
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy, HH:mm:ss");
+        LocalDateTime testTime = LocalDateTime.now().withNano(0);
+        FileWriter fw = new FileWriter(LocalStorage.get("dateLastRun.txt"));
+        fw.write(dtf.format(testTime));
+        fw.close();
+        //Make sure it works
+        assertEquals(scr.getLastRun(), testTime);
+        //Restore old time
+        FileWriter fw2 = new FileWriter(LocalStorage.get("dateLastRun.txt"), false);
+        fw2.write(realLastRun);
+        fw2.close();
     }
-
-//    @Test
-//    public void testRunAlgorithm() {
-//
-//    }
 }
