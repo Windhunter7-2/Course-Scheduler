58ec8bd7e42ab4c51372e6d653d20f86cc13c21f
jackmb <40642637+jackmb@users.noreply.github.com>
2020-04-27 12:08:41 -0400

    Renames JackTests.java to LastRunTest.java

--- src/test/java/CourseScheduler/LastRunTest.java
+++ src/test/java/CourseScheduler/LastRunTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 
-public class JackTests {
+public class LastRunTest {
 
     @Test
     public void testGetLastRun() throws IOException {
